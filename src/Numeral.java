b67699555c75f6d832641a29b14ca3b950a927bb
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-12-11 19:45:14 +0300

    v1.1

--- src/Numeral.java
+++ src/Numeral.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Numeral {
+    private ArrayList<String> num = new ArrayList<>();
+
+    public Numeral() {
+        num.add("0");
+    }
+
+    public Numeral(int x){
+        while(x!=0) {
+            int m = x%10;
+            num.add(String.valueOf(m));
+            x/=10;
+        }
+    }
+
+    public Numeral(String s){
+        for (int i = s.length(); i <= s.length(); i++) {
+            num.add(i, s.substring(i, i+1));
+        }
+    }
+
+    public Numeral sub(Numeral a, Numeral b) {
+        Numeral c = a;
+        for (int i = 0; i<a.num.size(); i++) {
+            if (a.num[i] < ) {
+            }
+        }
+    }
 }
